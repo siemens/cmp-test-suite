44c4b958a62d3f55f0638a353c47fbc8952b3ae2
Alexander Railean <alexander.railean@siemens.com>
2024-01-31 18:09:33 +0100

    Add ability to include comments in PEM files + close file after reading

--- resources/utils.py
+++ resources/utils.py
@@ -23,21 +23,36 @@ def buffer_length_must_be_at_least(data, length):
 
 
 def load_and_decode_pem_file(path):
-    """Load a base64-encoded PEM file, with or without a header, and return the decoded data
+    """Load a base64-encoded PEM file, with or without a header, ignore comments, and return the decoded data.
+
+    This is an augmented version of the PEM format, which allows one to add comments to the file, by starting the
+    line with a # character. This is purely a convenience for the user, and is not part of the standard.
 
     :param path: str, path to the file you want to load
     :returns: bytes, the data loaded from the file.
     """
-    # normally it should always have a header/trailer, but we'll be tolerant to that.
-    raw = open(path, 'r').readlines()
-    if '-----BEGIN' in raw[0]:
-        raw = ''.join(raw[1:-1])
+    # normally it should always have a header/trailer (aka "armour"), but we'll be tolerant to that.
+
+    filtered_lines = []
+    with open(path, 'r') as f:
+        raw = f.readlines()
+        # first do some cosmetic filtering
+        for line in raw:
+            if line.startswith('#'):  # remove comments
+                continue
+            elif line.strip() == '':  # remove blank lines
+                continue
+            else:
+                filtered_lines.append(line)
+
+    if '-----BEGIN' in filtered_lines[0]:
+        result = ''.join(filtered_lines[1:-1])
     else:
-        raw = ''.join(raw)
+        result = ''.join(filtered_lines)
 
     # note that b64decode doesn't care about \n in the string to be decoded, so we keep them to potentially improve
     # readability when debugging.
-    return b64decode(raw)
+    return b64decode(result)
 
 
 if __name__ == "__main__":
