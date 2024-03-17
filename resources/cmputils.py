2a317aa3928d22368a1e4c26877fbfd241e145f3
Alexander Railean <alexander.railean@siemens.com>
2024-03-17 03:08:59 +0100

    Add optional prefix to auto-generated transactionId

--- resources/cmputils.py
+++ resources/cmputils.py
@@ -658,7 +658,7 @@ def parse_csr(raw_csr):
     return csr
 
 
-def patch_transaction_id(pki_message, new_id=None):
+def patch_transaction_id(pki_message, new_id=None, prefix=None):
     """Patches the transactionId of a PKIMessage structure with a new ID, this is useful when you load a request
     from a file and send it multiple times to the CA. It would normally reject it because the transactionId is
     repeated - hence the patching.
@@ -666,12 +666,19 @@ def patch_transaction_id(pki_message, new_id=None):
     :param pki_message: pyasn1 PKIMessage structure, but raw DER-encoded blobs are also accepted, will be converted
                         automatically, this is to make it easier to use this function in RobotFramework
     :param new_id: optional bytes, new transactionId to use, will generate a random one by default
+    :param prefix: optional bytes or str, prefix to use for the transactionId, you will need this if you want the
+                   transactionId to be random, but still easily identifiable in the logs; we allow it to be a string
+                   so it can be passed directly from RobotFramework tests
     :returns: a pyasn1 PKIMessage structure with the updated transactionId
     """
     if type(pki_message) is bytes:
         pki_message = parse_pki_message(pki_message)
 
     new_id = new_id or os.urandom(16)
+
+    if prefix:
+        new_id = bytes(prefix, 'utf-8') + new_id
+
     wrapper_transaction_id = univ.OctetString(new_id).subtype(
         explicitTag=Tag(tagClassContext, tagFormatSimple, 4)
     )
