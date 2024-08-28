69533c396b7fdefe83e26a46cdc497c2e1d8c6e5
z0050kew <guiliano.lehmann@siemens.com>
2024-08-28 15:26:08 +0200

    fix test case test_errorutils

--- unit_tests/test_errorutils.py
+++ unit_tests/test_errorutils.py
@@ -1,8 +1,11 @@
 import unittest
 
 import cryptography.exceptions
+from pyasn1_alt_modules.rfc2459 import common_name
 
 import pycrypto_utils.verifying_utils
+from pycrypto_utils.cert_utils import generate_fresh_csr
+from pycrypto_utils.load_key_utils import load_rsa_key
 from resources import modifyutils
 from resources.cryptoutils import generate_csr, sign_csr, generate_rsa_keypair
 from cryptography.hazmat.primitives import serialization
@@ -22,11 +25,16 @@ class TestUtils(unittest.TestCase):
         :return: None
         """
 
-        csr = generate_csr("Hans Mustermann")
+        csr, key = generate_fresh_csr(common_name="CN=Hans", key=None)
 
+        #verify if the certificate is correct
+        pycrypto_utils.verifying_utils.verify_csr_signature(csr, key.public_key())
+        pycrypto_utils.verifying_utils.verify_csr_signature(csr, None)
 
         # Modify the common name (CN) in the CSR to "Hans MusterMann"
-        modified_csr = modifyutils.modify_csr_cn(der_data, new_cn="Hans MusterMann")
+        modified_csr = modifyutils.modify_csr_cn(csr, new_cn="Hans MusterMann")
 
         # Verify the signature of the modified CSR
-        self.assertRaises(pycrypto_utils.verifying_utils.verify_csr_signature(modified_csr), cryptography.exceptions.InvalidSignature)
+        with self.assertRaises(cryptography.exceptions.InvalidSignature):
+            pycrypto_utils.verifying_utils.verify_csr_signature(modified_csr, key.public_key())
+
