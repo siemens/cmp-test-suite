1c5d654cb45368aa88d117252dd094e1227a1fd5
z0050kew <guiliano.lehmann@siemens.com>
2024-08-28 13:48:34 +0200

    Add typingutils

---
+++ resources/typingutils.py
@@ -0,0 +1,21 @@
+from typing import Union, Any, Tuple
+
+from cryptography import x509
+from cryptography.hazmat.primitives.asymmetric.dsa import DSAPrivateKey
+from cryptography.hazmat.primitives.asymmetric.ec import EllipticCurvePrivateKey
+from cryptography.hazmat.primitives.asymmetric.rsa import RSAPrivateKey
+from cryptography.hazmat.primitives.asymmetric.dh import DHPrivateKey
+
+from pyasn1_alt_modules import rfc2986, rfc9480
+
+# Type alias for supported private key types
+ASYM_PRIVATE_KEY = Union[RSAPrivateKey, EllipticCurvePrivateKey, DSAPrivateKey, DHPrivateKey, bytes]
+
+
+# those types are Alias for the option a function can have.
+CSR_TYPE = Union[bytes, x509.CertificateSigningRequest, rfc2986.CertificationRequest]
+CERT_TYPE = Union[bytes, x509.Certificate, rfc9480.Certificate]
+
+
+# If a certificate is generated, a tuple is returned with the following type:
+CERT_GEN_RET = Tuple[x509.Certificate, ASYM_PRIVATE_KEY]
