9756d569f797ef268bc26fa657b0e7a9da9c089a
z0050kew <guiliano.lehmann@siemens.com>
2024-09-04 15:28:16 +0200

    Update httputils doc

--- resources/httputils.py
+++ resources/httputils.py
@@ -3,18 +3,18 @@ from pyasn1.error import PyAsn1Error
 from cmputils import parse_pki_message
 
 
-def request_contains_pki_message(data: requests.Response) -> False:
-    """Checks if a server returned a `PKIMessage` on failure.
+def http_response_contains_pki_message(data: requests.Response) -> False:
+    """Checks if a server returned a `rfc9480.PKIMessage` on failure.
 
-    The server might respond with an error status code, and in such cases, this function attempts to parse the response as a `PKIMessage`.
+    The server might respond with an error status code, and in such cases, this function attempts to parse the response as a `rfc9480.PKIMessage`.
     If the server response is empty or parsing of the `pyasn1` `PKIMessage` structure fails, the function returns `False`.
 
     Arguments:
-        data (requests.Response): The Response object.
+    - data: The Response object to parse.
 
     Returns:
-        pyasn1 parsed object: Represents the PKIMessage structure or None
-
+    - True if the response contains a valid PKIMessage.
+    - False if the response is empty or parsing fails.
     """
     if not data.content:
         return None
