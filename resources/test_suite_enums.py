895e580047dd878c939425dc8224780586a47fe4
z0050kew <guiliano.lehmann@siemens.com>
2024-09-05 15:48:23 +0200

    Rename cmp_enums to test_suite_enums

--- resources/test_suite_enums.py
+++ resources/test_suite_enums.py
@@ -1,5 +1,6 @@
-"""Creates an enum for the CMP protocol,
-making test cases more readable for users of the test suite.
+"""
+Defines Enums for use with the Certificate Management Protocol.
+These Enums make the test cases more readable for users of the test suite and facilitate comparisons and switches in the CMP protocol handling code.
 """
 
 from enum import Enum
