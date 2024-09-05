cd59f44952791f61a17749d8c18226c2758f5cb2
z0050kew <guiliano.lehmann@siemens.com>
2024-09-05 15:46:47 +0200

    Add generic verify

--- resources/verifyingutils.py
+++ resources/verifyingutils.py
@@ -2,7 +2,42 @@ from cryptography import x509
 from cryptography.hazmat.primitives.asymmetric import padding
 from robot.api.deco import not_keyword
 
-# TODO: Was changed in next Merge!
+
+def verify_signature(public_key: PublicKeySig, signature: bytes, data: bytes, hash_alg: str = None) -> None:
+    """Verify a digital signature using the provided public key and data.
+    Supports: (ECDSA, ED448, ED25519, RSA, DSA).
+
+    :param public_key: A `cryptography` public key object (e.g., Elliptic Curve, Ed25519, Ed448, DSA)
+                       used to verify the signature.
+    :param signature: The digital signature to be verified, provided as a byte sequence.
+    :param data: The original data that was signed, provided as a byte sequence.
+    :param hash_alg: Optional string representing the name of the hash algorithm to be used for verification
+                     (e.g., "sha256"). If not specified, the default algorithm for the given key type is used.
+
+    :raises
+        InvalidSignature: If an unsupported key type is provided.
+        ValueError: If the Signature is Invalid.
+    """
+    if hash_alg is not None:
+        hash_alg = hash_name_to_instance(hash_alg)
+    # isinstance(ed448.Ed448PrivateKey.generate(), EllipticCurvePrivateKey) → False
+    # so can check in this Order.
+    if isinstance(public_key, ec.EllipticCurvePublicKey):
+        public_key.verify(signature, data, ec.ECDSA(hash_alg))
+    elif isinstance(public_key, ed25519.Ed25519PublicKey):
+        public_key.verify(signature, data)
+    elif isinstance(public_key, ed448.Ed448PublicKey):
+        public_key.verify(signature, data)
+    elif isinstance(public_key, dsa.DSAPublicKey):
+        public_key.verify(signature, data, hash_alg)
+    elif isinstance(public_key, (x25519.X25519PublicKey, x448.X448PublicKey)):
+        raise ValueError(
+            f"Key type '{type(public_key).__name__}' is not used for signing or verifying signatures. It is used for key exchange."
+        )
+    else:
+        raise ValueError(f"Unsupported public key type: {type(public_key).__name__}.")
+
+
 
 
 @not_keyword
