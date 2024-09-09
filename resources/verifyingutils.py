1e94b6d0facaa697b2e45a445dea5637ac9f15a6
z0050kew <guiliano.lehmann@siemens.com>
2024-09-09 07:54:28 +0200

    Update verifing utils

--- resources/verifyingutils.py
+++ resources/verifyingutils.py
@@ -1,6 +1,6 @@
 from cryptography import x509
 from cryptography.hazmat.primitives import hashes
-from cryptography.hazmat.primitives.asymmetric import padding, ec, ed25519, ed448, dsa, x25519, x448
+from cryptography.hazmat.primitives.asymmetric import ec, ed25519, ed448, dsa, x25519, x448, rsa, padding
 from robot.api.deco import not_keyword
 
 from oid_mapping import hash_name_to_instance
@@ -17,16 +17,21 @@ def verify_signature(public_key: PublicKeySig, signature: bytes, data: bytes, ha
     :param data: The original data that was signed, provided as a byte sequence.
     :param hash_alg: Optional string representing the name of the hash algorithm to be used for verification
                      (e.g., "sha256"). If not specified, the default algorithm for the given key type is used.
-
     :raises
         InvalidSignature: If an unsupported key type is provided.
         ValueError: If the Signature is Invalid.
     """
-    if hash_alg is not None and not isinstance(hash_alg, hashes.HashAlgorithm):
+
+    if isinstance(hash_alg, hashes.HashAlgorithm):
+        pass
+    elif hash_alg is not None:
         hash_alg = hash_name_to_instance(hash_alg)
+
     # isinstance(ed448.Ed448PrivateKey.generate(), EllipticCurvePrivateKey) → False
     # so can check in this Order.
-    if isinstance(public_key, ec.EllipticCurvePublicKey):
+    if isinstance(public_key, rsa.RSAPublicKey):
+        public_key.verify(signature, data, padding=padding.PKCS1v15(), algorithm=hash_alg)
+    elif isinstance(public_key, ec.EllipticCurvePublicKey):
         public_key.verify(signature, data, ec.ECDSA(hash_alg))
     elif isinstance(public_key, ed25519.Ed25519PublicKey):
         public_key.verify(signature, data)
@@ -54,11 +59,12 @@ def verify_cert_signature(certificate: x509.Certificate):
         If the certificate's signature is not valid when verified against the provided or extracted public key.
     """
 
-
-    verify_signature(public_key=certificate.public_key(),
-                     signature=certificate.signature,
-                     data=certificate.tbs_certificate_bytes,
-                     hash_alg=certificate.signature_hash_algorithm)
+    verify_signature(
+        public_key=certificate.public_key(),
+        signature=certificate.signature,
+        data=certificate.tbs_certificate_bytes,
+        hash_alg=certificate.signature_hash_algorithm,
+    )
 
 
 @not_keyword
@@ -73,7 +79,9 @@ def verify_csr_signature(csr: x509.CertificateSigningRequest):
         If the csr's signature is not valid when verified against the provided or extracted public key.
     """
 
-    verify_signature(public_key=csr.public_key(),
-                     signature=csr.signature,
-                     data=csr.tbs_certrequest_bytes,
-                     hash_alg=csr.signature_hash_algorithm)
+    verify_signature(
+        public_key=csr.public_key(),
+        signature=csr.signature,
+        data=csr.tbs_certrequest_bytes,
+        hash_alg=csr.signature_hash_algorithm,
+    )
