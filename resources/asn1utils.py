fd09b337995474398e78dd4a4d53220b35af2232
Alexander Railean <alexander.railean@siemens.com>
2024-11-12 14:08:25 +0100

    Make pylint happy

--- resources/asn1utils.py
+++ resources/asn1utils.py
@@ -285,10 +285,6 @@ def is_bit_set(asn1_bitstring: BitString, bit_indices: Strint, exclusive: bool =
         | Is Bit Set | ${failInfo} | 1, 9                | ${True} |
 
     """
-    logging.info(f"exclusive: {exclusive} {type(exclusive)}")
-    logging.info(f"type: {type(asn1_bitstring)}")
-    logging.info(f"input: {bit_indices} type: {type(bit_indices)}")
-
     if not asn1_bitstring.isValue:
         raise ValueError("The Provided BitString has not set a Value!")
 
@@ -302,17 +298,17 @@ def is_bit_set(asn1_bitstring: BitString, bit_indices: Strint, exclusive: bool =
                 values = [int(x) for x in bit_indices.strip(" ").split(",")]
                 return _is_either_bit_set_in_bitstring(asn1_bitstring, values, exclusive=exclusive)
             return _is_bit_set_in_bitstring(asn1_bitstring, int(bit_indices.strip()), exclusive=exclusive)
-        else:
-            # gets the names of as single values.
-            values = bit_indices.strip(" ").split(",")
-            # gets the indices to the corresponding human-readable-names.
-            names = list(asn1_bitstring.namedValues.keys())
-            try:
-                bit_indices = [names.index(val) for val in values]
-            except ValueError as err:
-                raise ValueError(f"Provided names: {values} but allowed are: {names}") from err
 
-            return _is_either_bit_set_in_bitstring(asn1_bitstring, bit_indices, exclusive=exclusive)
+        # gets the names of as single values.
+        values = bit_indices.strip(" ").split(",")
+        # gets the indices to the corresponding human-readable-names.
+        names = list(asn1_bitstring.namedValues.keys())
+        try:
+            bit_indices = [names.index(val) for val in values]
+        except ValueError as err:
+            raise ValueError(f"Provided names: {values} but allowed are: {names}") from err
+
+        return _is_either_bit_set_in_bitstring(asn1_bitstring, bit_indices, exclusive=exclusive)
 
 
 def validate_structure(value):
@@ -344,7 +340,9 @@ def validate_query(value):
         raise argparse.ArgumentTypeError(f"Invalid ASN1Path query: {value}")
     return value
 
-if __name__ == '__main__':
+
+def main():
+    """Wrap the command-line interface"""
     parser = argparse.ArgumentParser(description='Query ASN1 structures using ASN1Path notation')
     parser.add_argument('filepath', type=pathlib.Path, help='Path to the file that contains the raw data,'
                                                             ' use `-` for stdin')
@@ -395,3 +393,7 @@ if __name__ == '__main__':
         print(result.asOctets())
     else:
         print(result)
+
+
+if __name__ == '__main__':
+    main()
