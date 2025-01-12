50cccd598b7afb3676e5a5234f6e3b918da63b2e
Default <guilianolehmann@live.de>
2025-01-12 23:02:43 +0100

    Add Migration Cert KeyUsage check

--- pq_logic/pq_validation_utils.py
+++ pq_logic/pq_validation_utils.py
@@ -3,51 +3,26 @@
 # SPDX-License-Identifier: Apache-2.0
 
 """Contains utility functions for validating PQ keys and certificates."""
+import logging
 
 from pyasn1.type import univ
 from pyasn1_alt_modules import rfc9480
+
+from pq_logic.keys.abstract_composite import AbstractCompositeKEMPublicKey
+from pq_logic.keys.abstract_hybrid_raw_kem_key import AbstractHybridRawPublicKey
+from pq_logic.keys.xwing import XWingPublicKey
 from resources import asn1utils
 from resources.certextractutils import get_field_from_certificate
 from resources.certutils import load_public_key_from_cert
 
-from pq_logic.keys.abstract_pq import PQPublicKey, PQSignaturePublicKey
+from pq_logic.keys.abstract_pq import PQPublicKey, PQSignaturePublicKey, PQKEMPublicKey
+from resources.oidutils import PQ_OID_2_NAME
 
 # https://www.ietf.org/archive/id/draft-ietf-lamps-kyber-certificates-06.txt
 # section 3:
 # When any of the ML-KEM AlgorithmIdentifier appears in the
 # SubjectPublicKeyInfo field of an X.509 certificate, the key usage
 # certificate extension MUST only contain `keyEncipherment`.
-
-
-def validate_pq_allowed_key_usages(cert: rfc9480.CMPCertificate, must_be_present: bool = False) -> None:
-    """Validate the key usage of a certificate with a PQ public key.
-
-    :param cert: The certificate to validate.
-    :param must_be_present: If the key usage must be present.
-    :return: None
-    """
-    public_key: PQPublicKey = load_public_key_from_cert(cert)  # type: ignore
-    key_usage = get_field_from_certificate(cert, extension="key_usage")
-
-    if key_usage is None and not must_be_present:
-        return
-
-    key_usage = asn1utils.get_set_bitstring_names(key_usage).split(", ")  # type: ignore
-
-    if isinstance(public_key, PQSignaturePublicKey):
-        ml_dsa_allowed = {"digitalSignature", "nonRepudiation", "keyCertSign", "cRLSign"}
-        ml_dsa_disallowed = {"keyEncipherment", "dataEncipherment", "keyAgreement", "encipherOnly", "decipherOnly"}
-
-        if not set(key_usage).issubset(ml_dsa_allowed):
-            raise ValueError(f"ML-DSA keyUsage must be one of: {ml_dsa_allowed}")
-        if set(key_usage) & ml_dsa_disallowed:
-            raise ValueError(f"ML-DSA keyUsage must not include: {ml_dsa_disallowed}")
-
-    else:
-        ml_kem_allowed = {"keyEncipherment"}
-        if set(key_usage) != ml_kem_allowed:
-            raise ValueError(f"ML-KEM keyUsage must only contain: {ml_kem_allowed}")
-
 
 NIST_ALGORITHMS = "2.16.840.1.101.3.4"
 SIG_ALGS = f"{NIST_ALGORITHMS}.3"
@@ -66,30 +41,66 @@ id_SLH_DSA_SHAKE_256S = univ.ObjectIdentifier(f"{SIG_ALGS}.30")
 id_SLH_DSA_SHAKE_256F = univ.ObjectIdentifier(f"{SIG_ALGS}.31")
 
 
-SLH_DSA_OID = {
-    id_SLH_DSA_SHA2_128S,
-    id_SLH_DSA_SHA2_128F,
-    id_SLH_DSA_SHA2_192S,
-    id_SLH_DSA_SHA2_192F,
-    id_SLH_DSA_SHA2_256S,
-    id_SLH_DSA_SHA2_256F,
-    id_SLH_DSA_SHAKE_128S,
-    id_SLH_DSA_SHAKE_128F,
-    id_SLH_DSA_SHAKE_192S,
-    id_SLH_DSA_SHAKE_192F,
-    id_SLH_DSA_SHAKE_256S,
-    id_SLH_DSA_SHAKE_256F,
-}
 
 
-def validate_slh_dsa_alg_id(alg_id: rfc9480.AlgorithmIdentifier) -> None:
-    """Validate the SLH-DSA signature algorithm identifier.
 
-    :param alg_id: The SLH-DSA `AlgorithmIdentifier`.
+def validate_migration_alg_id(alg_id: rfc9480.AlgorithmIdentifier) -> None:
+    """Validate a post-quantum or hybrid algorithm identifier.
+
+    :param alg_id: The `AlgorithmIdentifier`.
     :raises ValueError: If the `parameters` field must be absent.
     """
-    if alg_id["parameters"] in SLH_DSA_OID:
-        if alg_id["algorithm"].isValue:
-            raise ValueError(
-                f"The 'parameters' field is not allowed to " f"be set for algorithm OID: {alg_id['parameters']}"
-            )
+
+    if alg_id["algorithm"] not in PQ_OID_2_NAME:
+        if alg_id["parameters"].isValue:
+            name = PQ_OID_2_NAME.get(alg_id["algorithm"])
+            name = name or PQ_OID_2_NAME.get(str(alg_id["algorithm"]))
+            raise ValueError(f"The Post-Quantum algorithm identifier {name} does not `allow` the parameters"
+                             f" field to be set: {alg_id['parameters']}")
+
+
+
+
+def validate_certificate_key_usage(cert: rfc9480.CMPCertificate) -> None:
+    """Validate the key usage of a certificate with a PQ public key.
+
+    :param cert: The certificate to validate.
+    :return: None
+    """
+
+    public_key: PQPublicKey = load_public_key_from_cert(cert)  # type: ignore
+    key_usage = get_field_from_certificate(cert, extension="key_usage")
+
+    if key_usage is None:
+        logging.info("Key usage extension was not present parsed certificate.")
+        return
+
+    key_usage = asn1utils.get_set_bitstring_names(key_usage).split(", ")  # type: ignore
+
+    sig_usages = {"digitalSignature", "nonRepudiation", "keyCertSign", "cRLSign"}
+
+    if isinstance(public_key, PQSignaturePublicKey):
+        ml_dsa_disallowed = {"keyEncipherment", "dataEncipherment", "keyAgreement", "encipherOnly", "decipherOnly"}
+
+        if not set(key_usage).issubset(sig_usages):
+            raise ValueError(f"The post-quantum {public_key.name} keyUsage must be one of: {sig_usages}")
+        if set(key_usage) & ml_dsa_disallowed:
+            raise ValueError(f"ML-DSA keyUsage must not include: {ml_dsa_disallowed}")
+
+    elif isinstance(public_key, PQKEMPublicKey):
+        ml_kem_allowed = {"keyEncipherment"}
+        if set(key_usage) != ml_kem_allowed:
+            raise ValueError(f"ML-KEM keyUsage must only contain: {ml_kem_allowed}")
+
+    elif isinstance(public_key, AbstractCompositeKEMPublicKey):
+        pq_allowed = {"keyEncipherment"}
+        if set(key_usage) != pq_allowed:
+            raise ValueError(f"A CompositeKEM public key MUST have only the keyUsage 'keyEncipherment'.")
+
+    elif isinstance(public_key, XWingPublicKey):
+        pq_allowed = {"keyEncipherment"}
+        if set(key_usage) != pq_allowed:
+            raise ValueError("A XWing public key MUST have only the keyUsage 'keyEncipherment'.")
+    
+    else:
+        raise ValueError(f"Unsupported public key type: {type(public_key)}")
