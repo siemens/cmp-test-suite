28a14f70ea04b052c68196cdb806f7de923b99f3
Default <guilianolehmann@live.de>
2025-01-27 08:26:16 +0100

    Add noqa to __init__ file

--- pq_logic/hybrid_sig/chameleon_certs/__init__.py
+++ pq_logic/hybrid_sig/chameleon_certs/__init__.py
@@ -1,6 +1,6 @@
+# noqa D104 Missing docstring in public package
 # SPDX-FileCopyrightText: Copyright 2024 Siemens AG
 #
 # SPDX-License-Identifier: Apache-2.0
 
-# noqa D104 Missing docstring in public package
 
