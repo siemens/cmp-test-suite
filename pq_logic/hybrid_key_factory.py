d4509e81232d8d4d10e0b5de8b674b1dad5b0a5c
Default <guilianolehmann@live.de>
2025-01-27 08:19:52 +0100

    Fix complete hybrid_key_factory

--- pq_logic/hybrid_key_factory.py
+++ pq_logic/hybrid_key_factory.py
@@ -2,6 +2,8 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
+"""Factory for creating hybrid keys based on pq and traditional components."""
+
 from typing import List, Optional
 
 from cryptography.hazmat.primitives.asymmetric import ec, ed448, ed25519, rsa, x448, x25519
@@ -169,9 +171,7 @@ def _get_kem_comp_combinations(
 
 
 class HybridKeyFactory:
-    """
-    Factory for creating composite keys based on classical key types.
-    """
+    """Factory for creating composite keys based on classical key types."""
 
     @staticmethod
     def from_keys(algorithm: str, pq_key, trad_key):
@@ -309,6 +309,7 @@ class HybridKeyFactory:
 
     @staticmethod
     def get_all_kem_coms():
+        """Return all possible hybrid kem combinations."""
         return ALL_COMPOSITE_KEM_COMBINATIONS + ALL_CHEMPAT_POSS_COMBINATIONS + [{"xwing"}]
 
     @staticmethod
@@ -326,8 +327,11 @@ class HybridKeyFactory:
     ):
         """Generate a hybrid key.
 
+        :param algorithm: The hybrid key algorithm.
         :param pq_name: Name of the post-quantum key algorithm.
         :param trad_name: Name of the traditional key algorithm.
+        :param length: Length of the RSA key.
+        :param curve: Curve of the EC key.
         :return: Instance of a Hybrid key.
         """
         if algorithm == "xwing":
