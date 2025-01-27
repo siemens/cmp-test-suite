27d8af93e7fc9588b6236ae39a26e2265b441176
Default <guilianolehmann@live.de>
2025-01-27 11:54:08 +0100

    Add init

---
+++ unit_tests/tests_experimental_and_mock_ca/__init__.py
@@ -0,0 +1,8 @@
+"""
+Contains Custom Ideas how the newly developed mechanisms can be used in CMP and to issue
+a certificate.
+
+As an example can use Hybrid keys in CMP to issue a certificate,
+like Catalyst or Composite keys.
+"""
+
