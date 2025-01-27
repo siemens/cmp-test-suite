e8d80000426c8185edf761e75b4ad534d4abf6cd
Default <guilianolehmann@live.de>
2025-01-27 10:40:46 +0100

    Add _verify_pop_signature

--- resources/ca_ra_utils.py
+++ resources/ca_ra_utils.py
@@ -444,6 +444,43 @@ def get_public_key_from_cert_req_msg(cert_req_msg: rfc4211.CertReqMsg) -> Public
     old_spki["subjectPublicKey"] = spki["subjectPublicKey"]
 
     return load_public_key_from_spki(old_spki)
+
+
+def _verify_pop_signature(
+    pki_message: rfc9480.PKIMessage,
+) -> None:
+    """Verify the POP signature in the PKIMessage.
+
+    :param pki_message: The PKIMessage to verify the POP signature for.
+    :return: `None`.
+    """
+    body_name = pki_message["body"].getName()
+
+    try:
+        cert_req_msg = get_cert_req_msg_from_pkimessage(pki_message)
+        popo: rfc4211.ProofOfPossession = cert_req_msg["popo"]
+        if not popo["signature"].isValue:
+            raise ValueError("POP signature is missing in the PKIMessage.")
+
+        popo_sig = popo["signature"]
+        public_key = get_public_key_from_cert_req_msg(cert_req_msg)
+        verify_signature_with_alg_id(
+            public_key=public_key,
+            alg_id=popo_sig["algorithmIdentifier"],
+            data=encoder.encode(cert_req_msg["certReq"]),
+            signature=popo_sig["signature"].asOctets(),
+        )
+
+        if cert_req_msg["regInfo"].isValue:
+            logging.debug("regInfo is present in the CertReqMsg,but server logic is not supported yet.")
+
+    except pyasn1.error.PyAsn1Error as err:
+        raise BadAsn1Data("Failed to encode the CertRequest.", overwrite=True) from err
+
+    except InvalidSignature as err:
+        raise BadPOP(f"POP verification for `{body_name}` failed.") from err
+
+
 def prepare_enc_key(env_data: rfc5652.EnvelopedData, explicit_tag: int = 0) -> rfc9480.EncryptedKey:
     """Prepare an EncryptedKey structure by encapsulating the provided EnvelopedData.
 
