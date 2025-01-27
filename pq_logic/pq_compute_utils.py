69ae318ff3da4e9c30a8df9eb0d1aa41ad8e0633
Default <guilianolehmann@live.de>
2025-01-27 11:13:14 +0100

    Update verify_signature_with_alg_id

--- pq_logic/pq_compute_utils.py
+++ pq_logic/pq_compute_utils.py
@@ -178,20 +178,39 @@ def may_extract_alt_key_from_cert(
     return None
 
 
+@not_keyword
+def verify_signature_with_alg_id(public_key, alg_id: rfc9480.AlgorithmIdentifier, data: bytes, signature: bytes):
+    """Verify the provided data and signature using the given algorithm identifier.
 
+    Supports traditional-, pq- and composite signature algorithm.
 
+    :param public_key: The public key object used to verify the signature.
+    :param alg_id: An `AlgorithmIdentifier` specifying the algorithm and any
+                   associated parameters for signature verification.
+    :param data: The original message or data whose signature needs verification,
+                 as a byte string.
+    :param signature: The digital signature to verify, as a byte string.
+
+    :raises ValueError: If the algorithm identifier is unsupported or invalid.
+    :raises InvalidSignature: If the signature does not match the provided data
+                              under the given algorithm and public key.
     """
-    if alt_issuer_key is None:
-        alt_issuer_key = may_extract_alt_key_from_cert(issuer_cert, other_certs=other_certs)
-        if alt_issuer_key is None:
-            raise ValueError("No alternative issuer key found.")
+    oid = alg_id["algorithm"]
 
-    alt_pub_key = validate_alt_pub_key_extn(cert)
-    if check_alt_sig:
-        validate_alt_sig_extn(cert, alt_pub_key, alt_issuer_key)
+    if oid in CMS_COMPOSITE_OID_2_NAME:
+        name: str = CMS_COMPOSITE_OID_2_NAME[oid]
+        use_pss = name.endswith("-pss")
+        pre_hash = name.startswith("hash-")
+        public_key: CompositeSigCMSPublicKey
+        public_key.verify(data=data, signature=signature, use_pss=use_pss, pre_hash=pre_hash)
 
-    public_key = load_public_key_from_cert(issuer_cert)
-    data = encoder.encode(cert["tbsCertificate"])
-    alg_id = cert["tbsCertificate"]["signature"]
-    signature = cert["signature"].asOctets()
-    verify_signature_with_alg_id(public_key=public_key, data=data, signature=signature, alg_id=alg_id)
+    elif oid in RSASSA_PSS_OID_2_NAME:
+        return verify_rsassa_pss_from_alg_id(public_key=public_key, data=data, signature=signature, alg_id=alg_id)
+
+    elif oid in PQ_OID_2_NAME or str(oid) in PQ_OID_2_NAME or oid in MSG_SIG_ALG:
+        hash_alg = get_hash_from_oid(oid, only_hash=True)
+        verify_signature(public_key=public_key, signature=signature, data=data, hash_alg=hash_alg)
+    else:
+        raise ValueError(f"Unsupported public key type: {type(public_key).__name__}.")
+
+
