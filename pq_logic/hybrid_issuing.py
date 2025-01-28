77dd5688585417e932e15b168829ef54acaf0ff3
Default <guilianolehmann@live.de>
2025-01-28 10:52:23 +0100

    Update hybrid_issuing doc

---
+++ pq_logic/hybrid_issuing.py
@@ -0,0 +1,17 @@
+# SPDX-FileCopyrightText: Copyright 2024 Siemens AG
+#
+# SPDX-License-Identifier: Apache-2.0
+
+"""Allows issuing certificates with hybrid keys/and mechanisms.
+
+##############################################################################
+
+Warning:
+-------
+This does not official ideas how to issue certificates with hybrid keys,
+like Catalyst. Some are just Ideas how someone could use the newly developed
+mechanisms to issue a certificate.
+##############################################################################
+
+"""
+
