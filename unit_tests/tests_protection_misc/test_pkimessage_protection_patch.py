caac1ffd6e7d25f5e4fb7a26734bb69d00f95c6f
Default <guilianolehmann@live.de>
2025-01-29 11:56:17 +0100

    Add unittest, to see if patching still works for protection

---
+++ unit_tests/tests_protection_misc/test_pkimessage_protection_patch.py
@@ -0,0 +1,38 @@
+import unittest
+
+from resources.certbuildutils import build_csr
+from resources.certutils import parse_certificate
+from resources.cmputils import build_p10cr_from_csr
+from resources.keyutils import load_private_key_from_file
+from resources.protectionutils import protect_pkimessage
+from resources.utils import load_and_decode_pem_file
+
+
+class TestPKIMessagePatchCertificate(unittest.TestCase):
+
+
+    def test_patch_with_cert(self):
+        """
+        GIVEN a PKIMessage
+        WHEN the PKIMessage is protected,
+        THEN the PKIMessage is protected.
+        """
+        rsa_key = load_private_key_from_file("data/keys/private-key-rsa.pem", password=None)
+        cert = parse_certificate(load_and_decode_pem_file("data/unittest/bare_certificate.pem"))
+        csr = build_csr(
+            signing_key=rsa_key,
+        )
+        p10cr = build_p10cr_from_csr(
+            csr=csr,
+            exclude_fields="senderKID,sender",
+        )
+        _ = protect_pkimessage(
+            p10cr,
+            private_key=rsa_key,
+            protection="signature",
+            cert=cert
+        )
+
+
+
+
