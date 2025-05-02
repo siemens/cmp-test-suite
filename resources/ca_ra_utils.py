cf75bf8416e2529edb5e7a1e8995e5e382300c9e
Default <guilianolehmann@live.de>
2025-05-02 09:03:36 +0200

    Fix arg name, line-too-long

--- resources/ca_ra_utils.py
+++ resources/ca_ra_utils.py
@@ -712,7 +712,7 @@ def prepare_private_key_for_kga(
         version_num = int(version_num)
 
     if new_private_keys is None:
-        private_keys = prepare_invalid_kga_private_key(
+        new_private_keys = prepare_invalid_kga_private_key(
             new_private_key=new_private_key,
             invalid_operation=invalid_kga_operation,
             key_save_type=key_save_type or "raw",
@@ -723,7 +723,7 @@ def prepare_private_key_for_kga(
         signing_key=kga_key,
         sig_hash_name=None,  # will automatically be set to the hash algorithm used for the KGA key.
         cert=kga_cert_chain[0],
-        private_keys=private_keys,
+        private_keys=new_private_keys,
         cert_chain=kga_cert_chain,
     )
     signed_data_der = encoder.encode(signed_data)
@@ -4310,8 +4310,8 @@ def build_kga_cmp_response(  # noqa D417 undocumented-param
         - `extensions`: The extensions to be added to the build certificate. Defaults to `None`.
         - `key_save_type`: Whether to save the PQ-key as `seed`, `raw` or `seed_and_raw`. Defaults to `raw`.
         - `default_key_type`: The default key type to generate for the client request. Defaults to `rsa`.
-        - `new_private_key` (List[rfc5958.OneAsymmetricKey], rfc5958.OneAsymmetricKey): The new private key to use for the KGA request. \
-        Defaults to `None`.
+        - `new_private_key` (List[rfc5958.OneAsymmetricKey], rfc5958.OneAsymmetricKey): The new private key to use \
+        for the KGA request. Defaults to `None`.
 
     Returns:
     -------
