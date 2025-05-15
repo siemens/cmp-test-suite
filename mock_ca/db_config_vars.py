ef75bd876b0edc8e334c2bbb68ae49b1ea4dec88
Default <guilianolehmann@live.de>
2025-05-15 16:29:58 +0200

    Add licences

--- mock_ca/db_config_vars.py
+++ mock_ca/db_config_vars.py
@@ -1,3 +1,7 @@
+# SPDX-FileCopyrightText: Copyright 2025 Siemens AG
+#
+# SPDX-License-Identifier: Apache-2.0
+
 """Dataclasses for configuration variables used by the MockCA."""
 
 from abc import ABC
--- unit_tests/tests_cert_related/tests_prepare_extensions/test_prepare_private_key_usage_period.py
+++ unit_tests/tests_cert_related/tests_prepare_extensions/test_prepare_private_key_usage_period.py
@@ -1,3 +1,7 @@
+# SPDX-FileCopyrightText: Copyright 2025 Siemens AG
+#
+# SPDX-License-Identifier: Apache-2.0
+
 import unittest
 from datetime import datetime, timezone
 
