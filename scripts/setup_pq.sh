9c5cb0ccc5bad745d5686f2bc25eaca0a3e179ae
Default <guilianolehmann@live.de>
2025-06-13 08:00:32 +0200

    Update setup, dockerfile to use Guiliano99/liboqs-python-stateful-sig.

--- scripts/setup_pq.sh
+++ scripts/setup_pq.sh
@@ -6,12 +6,12 @@
 sudo apt update
 sudo apt install libssl-dev
 sudo apt install cmake
-git clone --depth=1 https://github.com/open-quantum-safe/liboqs-python
+git clone --depth=1 https://github.com/Guiliano99/liboqs-python-stateful-sig.git
 # shellcheck disable=SC2164
-cd liboqs-python
+cd liboqs-python-stateful-sig
 pip install .
 # shellcheck disable=SC2103
 cd ..
 # sanity check, if an error occurs, the script will try to install itself,
 # which resolves the problem.
-python liboqs-python/examples/kem.py
+python liboqs-python-stateful-sig/examples/kem.py
