6fc98084dbb6b12cd2be5214bf7c39b85da35a09
Default <guilianolehmann@live.de>
2025-06-28 18:08:17 +0200

    Update generate_xmss_keys.py.

--- generate_xmss_keys.py
+++ generate_xmss_keys.py
@@ -21,9 +21,16 @@ ALL_REQUEST_BODY_NAMES = [
     "batch-inner-ccr",
 ]
 
+print("Generating XMSS keys...")
+print("This may take a while, please be patient...")
+print("All keys will be saved in the `data/keys/xmss_xmssmt_key_verbose` directory.")
+if not os.path.exists("data/keys/xmss_xmssmt_key_verbose"):
+    os.makedirs("data/keys/xmss_xmssmt_key_verbose", exist_ok=True)
 
-for alg in CombinedKeyFactory.get_stateful_sig_algorithms()["xmss"]:
-    for body_name in ALL_REQUEST_BODY_NAMES:
+print("All available algorithms:", CombinedKeyFactory.get_stateful_sig_algorithms()["xmss"])
+
+for body_name in ALL_REQUEST_BODY_NAMES:
+    for alg in CombinedKeyFactory.get_stateful_sig_algorithms()["xmss"]:
         for reason in ["bad_pop", "popo", "bad_params", "bad_key_size", "exhausted"]:
             dir_name = "data/keys/xmss_xmssmt_key_verbose"
             path = os.path.join(dir_name, f"{alg.lower()}_{body_name}_{reason}.pem")
@@ -31,10 +38,8 @@ for alg in CombinedKeyFactory.get_stateful_sig_algorithms()["xmss"]:
                 continue
 
             key = generate_key(alg.lower())
-            save_key(
-                key, path
-            )
+            save_key(key, path)
 
             print("Finished Key: ", f"{alg.lower()}_{body_name}_{reason}")
 
-    print("Finished algorithm", alg.lower())
+        print("Finished algorithm", alg.lower(), "for body name", body_name)
