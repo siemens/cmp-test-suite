6ab92feb5db8f893b462117db195c90441989aa2
Guiliano99 <guilianolehmann@live.de>
2026-01-17 13:12:09 +0100

    Add HASH_ALG_TO_STRENGTH mapping in security_utils

--- resources/security_utils.py
+++ resources/security_utils.py
@@ -3,7 +3,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-"""Contains security related utility functions, like getting the bit string of a used key."""
+"""Contain security-related utility functions, like getting the bit string of a used key."""
 
 
 # Security strength values follow NIST SP 800-57 Part 1 Revision 5, Tables 2 and 4.
@@ -15,4 +15,18 @@ _NIST_LEVEL_TO_STRENGTH = {
     3: 192,
     4: 256,
     5: 256,
+}
+
+HASH_ALG_TO_STRENGTH = {
+    "sha1": 80,
+    "sha224": 112,
+    "sha256": 128,
+    "sha384": 192,
+    "sha512": 256,
+    "sha3_224": 112,
+    "sha3_256": 128,
+    "sha3_384": 192,
+    "sha3_512": 256,
+    "shake128": 128,  # Uses in CMP 32 Byte as output size. According to RFC 9481.
+    "shake256": 256,  # Uses in CMP 64 Byte as output size. According to RFC 9481.
 }
