9f421a45a224c545259dfb4f686301c60a5f2743
Guiliano99 <guilianolehmann@live.de>
2026-01-19 08:20:55 +0100

    Refactor `compare_pyasn1_objects` to return detailed comparison results, replacing `print` statements with a list of differences.

--- unit_tests/utils_for_test.py
+++ unit_tests/utils_for_test.py
@@ -441,20 +441,20 @@ def pretty_print_extensions(extensions: rfc9480.Extensions) -> List[str]:
         out.append(entry)
     return out
 
-def compare_pyasn1_objects(first: base.Asn1Type, second: base.Asn1Type) -> bool:
+def compare_pyasn1_objects(first: base.Asn1Type, second: base.Asn1Type) -> Tuple[bool, List[str]]:
     """Compare if two pyasn1 structures, by first encoding them and then compare the bytes.
 
-    :param first: The first object to compare.
-    :param second: The second object to compare.
-    :return: True if the structures are identical; False otherwise.
+    :param first: The first object to compare with the second.
+    :param second: The second object to compare with the first.
+    :return: `True` if the structures are identical; `False` otherwise which includes a list of differences.
     """
     result = encoder.encode(first) == encoder.encode(second)
+    out: List[str] = []
     if not result:
         for field in first.keys():  # type: ignore
             if encoder.encode(first[field]) != encoder.encode(first[field]):  # type: ignore
-                print(f"{field}: {first[field].prettyPrint()} != {second[field].prettyPrint()}")  # type: ignore
-
-    return result
+                out.append(f"{field}: {first[field].prettyPrint()} != {second[field].prettyPrint()}")  # type: ignore
+    return result, out
 
 
 @not_keyword
