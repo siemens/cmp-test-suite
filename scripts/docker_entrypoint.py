50c9a43a47195889d54195711da1210a130b7418
Guiliano99 <guilianolehmann@live.de>
2026-03-26 20:36:13 +0100

    fix(docker): Update smoke test path to reference the correct directory

--- scripts/docker_entrypoint.py
+++ scripts/docker_entrypoint.py
@@ -242,8 +242,8 @@ def main():
         additional_args = ""
 
     if args.smoke:
-        # Run the smoke test in scripts/smoke.robot, as well as any other test with the `smoke` tag
-        command = "robot --pythonpath=./ --outputdir=/report --include smoke scripts/smoke.robot"
+        # Run the smoke test in tests/smoke.robot, as well as any other test with the `smoke` tag
+        command = "robot --pythonpath=./ --outputdir=/report --include smoke tests/smoke.robot"
     elif args.minimal:
         # A minimal batch of tests that only need to know the server's address and nothing else, it is the easiest
         # way to get a taste of what the test suite can do while still doing some actual work with a real server.
